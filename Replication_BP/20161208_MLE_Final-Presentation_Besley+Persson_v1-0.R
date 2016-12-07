db90e8f653b2015132bfc3c67ad2af51a57695d7
piton3853 <piton3853@gmail.com>
2016-12-07 12:56:59 -0500

    20161207_1256_mac commit updated datafile and script

--- Replication_BP/20161208_MLE_Final-Presentation_Besley+Persson_v1-0.R
+++ Replication_BP/20161208_MLE_Final-Presentation_Besley+Persson_v1-0.R
@@ -10,11 +10,15 @@
 # SOURCE:  Besley & Persson (2009);
 # DATASET Source:  https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UN54HD
 
-###############################
-#####       PREAMBLE      #####
-###############################
+################################################
+###             PREAMBLE                     ###
+################################################
+if(T){                            # This collapses the PREAMBLE
 # Clear & Reset
-if(T){rm(list=ls()); cat("\014")}
+# Clear Environment
+rm(list=ls())
+# Clear Console
+cat("\014")
 
 ###############################
 #####       PACKAGES      #####
@@ -22,7 +26,7 @@ if(F){
   install.packages("pacman", repos='http://cran.us.r-project.org')
   pacman::p_load("foreign", "stargazer", "dplyr", "ggplot2", "devtools",
                  "obsval","mvtnorm","arm","vioplot","lmtest","knitr","readstata13",
-                 "moments","MASS","XLConnect","xlsx","plm","apsrtable","repmis","gridExtra")
+                 "moments","MASS","XLConnect","xlsx","plm","apsrtable","repmis","gridExtra","Hmsic")
 }                #####
 ###############################
 
@@ -44,23 +48,148 @@ library(knitr)
 library(readstata13)
 library(moments)
 library(MASS)
+library(repmis)
+library(gridExtra)
+library(apsrtable)
+library(plm)
+library(Hmisc)
 }
 ###############################
 
 ###############################
 #####         DATA        #####
 if(T){
-  setwd("~/Documents/GitHubRepo/729_Reed_MLE_git/Replication_besley_persson")
-  repcw01 <- read.table(file = "BP.formatted.tab", header = TRUE, sep = "\t")
-  save(repcw01, file = "repcw01.RData")
-  source_data("https://github.com/piton3853/2016F_888C_Final-Replication/blob/master/rossdata01.RData?raw=true")
-  #write.xlsx(rossdata01, "~/Documents/GitHubRepo/2016F_888C_Final-Replication/ross/Replication data for The Oil Curse - Ross 2012.xlsx")
-  #View(rossdata01)
+  setwd("~/Documents/GitHubRepo/729_Reed_MLE_git/Replication_BP")
+  #repcw01 <- read.table(file = "BP.formatted.tab", header = TRUE, sep = "\t")
+  #repcw02 <- read.dta(file = "world panel 28 nov9.dta")
+  #repcw03 <- read.table(file = "bp_exact_for_analysis.tab", header = T, sep = "\t")
+  #repcw04 <- read.table(file = "replication.tab", header = T, sep = "\t")
+  #save(repcw01, file = "repcw01.RData")
+  #save(repcw02, file = "repcw02.RData")
+  #save(repcw03, file = "repcw03.RData")
+  #save(repcw04, file = "repcw04.RData")
+  #source_data("https://github.com/piton3853/729_Reed_MLE_git/blob/master/Replication_BP/repcw01.RData")
+  load('repcw01.RData')
+  #load('repcw02.RData')
+  #load('repcw03.RData')
+  #load('repcw04.RData')
+  #write.dta(repcw01,"~/Documents/GitHubRepo/729_Reed_MLE_git/Replication_BP/repcw01.dta")
+  #View(repcw01)
 }
 ###############################
+}                             # This is the end of the PREAMBLE
 
-###############################
-#####   BUILD DATAFRAMES  #####
-###############################
+###########################
+###  BUILD DATAFRAMES   ###
+###########################
+# Create named factors for DV
+#repcw01$rep_civwar_DV <- as.factor(repcw01$rep_civwar_DV)
+#str(repcw01$rep_civwar_DV)
+#str(repcw01$p_r_cw)
+
+############################
+###  MODEL REPLICATION   ###
+############################
+if(T){                    # This collapses the Model Replication Section
+require(MASS)
+#repcw01 <- repcw01 %>%  mutate(p_r_cw = revalue(rep_civwar_DV, c("0" = "Peace","1" = "Repression","2" = "Civil War")))
+#repcw01$p_r_cw <- ordered(as.factor(repcw01$p_r_cw))
+#save(repcw01, file = "repcw01.RData")
+model.repCivWar.01 <- polr(p_r_cw ~ logGDPpc + parliament + major_oil + major_primary + disaster,
+             data = repcw01,
+             Hess=TRUE, 
+             method="logistic"); summary(model.repCivWar.01)
+
+}                     # This is the end of the MODEL REPLICATION Section
+
+############################
+###    LOOK @ THE DATA   ###
+############################
+if(T){                      # This collapses the LOOK @ THE DATA Section
+if(F){
+ggplot(repcw01, aes(x=logGDPpc, fill=p_r_cw)) +
+  geom_histogram(binwidth=.5, alpha=.5, position="identity")
+} # this is a histogram plot
+
+# View Data
+cdplot(repcw01$p_r_cw~repcw01$logGDPpc,ylab = "Peace,Repression,&CivilWar",xlab = "logGDP")
+
+}                       # This is the end of the LOOK @ THE DATA Section
+
+############################
+###    OBSERVED VALUES   ###
+############################
+if(T){                               # This collapses the OBSERVED VALUES Section 
+# Show the effect of moving `logGDPpc` from its minimum to maximum 
+# value on the probability of observing the four categories of the 
+# dependent variable, `p_r_cw` (Peace, Repression, or Civil War).  
+# Use the observed value approach and violin plots to show the effect of `logGDPpc`.
+
+# observed values for ordered logit -----------------------------------------
+beta <- c(model.repCivWar.01$coefficients, model.repCivWar.01$zeta); head(beta)
+model.repCivWar.01_vcov <- vcov(model.repCivWar.01)
+n.draws <- 500
+set.seed(42)
+sim.coefs <- rmvnorm(n.draws, beta, vcov(model.repCivWar.01)); head(sim.coefs)
+
+#####reorder the columns - so the simulated coefficients are in the same order as the model#
+sim.coefs <- sim.coefs[, c(names(model.repCivWar.01$coefficients),names(model.repCivWar.01$zeta))];head(sim.coefs)
+
+# p_lower.1 <- numeric(n.draws) # this was Neil's Code that conflicted with Bill's
+# p_lower.2 <- numeric(n.draws) # this was Neil's Code that conflicted with Bill's
+
+# p_upper.1 <- numeric(n.draws) # this was Neil's Code that conflicted with Bill's
+# p_upper.2 <- numeric(n.draws) # this was Neil's Code that conflicted with Bill's
+
+n.obs <- length(repcw01[[1]])
+}                 # This is the end of the OBSERVED VALUES front Section
+
+# Latent probability loop -
+for(i in 1:n.draws){ 
+  
+  # For the current set of coefficients, calculate a
+  # latent probability for all observations using observed values
+  
+  # first, set up vectors to store our linear predictors
+  Xb.1 	<- numeric(n.obs)           # Cut Point 1
+  Xb.2 	<- numeric(n.obs)           # Cut Point 2
+
+
+
+# Observed Values for loop
+  # second, for current set of coefs, loop through each observation
+  # and calculate mean, high and low linear predictors
+  for(j in 1:n.obs){
+    ###reordered columns are now numbered consitently here ###
+    ###   1   ###
+    Xb.1[j] <-  sim.coefs[i,6]*1 +                               # Cutpoint 6   Identify these from sim.coefs
+                sim.coefs[i,7]*0 -                               # Cutpoint 7   Identify these from sim.coefs
+                  (sim.coefs[i,1]*as.numeric(logGDPpc) +                              # 1
+                    sim.coefs[i,2]*as.numeric(repcw01$parliament[j]) +    # 2
+                    sim.coefs[i,3]*as.numeric(repcw01$major_oil[j]) +     # 3
+                    sim.coefs[i,4]*as.numeric(repcw01$major_primary[j]) + # 4
+                    sim.coefs[i,5]*as.numeric(repcw01$disaster[j]))       # 5
+    ###   2   ###
+    Xb.2[j] <-  sim.coefs[i,6]*0 +                               # Cutpoint 6   Identify these from sim.coefs
+                sim.coefs[i,7]*1 -                               # Cutpoint 7   Identify these from sim.coefs
+                  (sim.coefs[i,1]*as.numeric(logGDPpc) +                              # 1
+                    sim.coefs[i,2]*as.numeric(repcw01$parliament[j]) +    # 2
+                    sim.coefs[i,3]*as.numeric(repcw01$major_oil[j]) +     # 3
+                    sim.coefs[i,4]*as.numeric(repcw01$major_primary[j]) + # 4
+                    sim.coefs[i,5]*as.numeric(repcw01$disaster[j]))       # 5
+
+  # calculate probability of being in category 1-3 for each observation
+  p1 <- plogis(Xb.1)
+  p2 <- plogis(Xb.2)-plogis(Xb.1)
+  p3 <- plogis(Xb.2)
+
+  # average probability across all observations
+  p.1[i] <- mean(p1)
+  p.2[i] <- mean(p2)
+  p.3[i] <- mean(p3r)
+  }
+}
+
+
 
 
